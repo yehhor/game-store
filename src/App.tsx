d5a4033d77cf8ba73049a80c2d360735854d464e
Yehor <yehhor.k@gmail.com>
2022-06-27 20:17:33 +0300

    refactor cart layout

--- src/App.tsx
+++ src/App.tsx
@@ -11,28 +11,21 @@ import 'swiper/css'
 import {Cart} from "./components/Cart";
 import {useClickOutside} from "./components/useClickOutside";
 import {BASE_URL} from "./index";
+import {Login} from "./Pages/Login/Login";
 
 
 function App() {
-    const [cartOpen, setCartVisibility] = useState(false)
     const location = useLocation();
-    const ref = React.createRef<HTMLDivElement>()
-    console.log('app rendered');
     //todo is it ok?
-    useClickOutside(ref, () => {
-        console.log('out');
-        if(cartOpen) {
-            setCartVisibility(false)
-        }
-    })
+
 
     return (
         <div className="App">
-            <Header handleCartClick={() => setCartVisibility(!cartOpen)}/>
-            <Cart ref={ref} cartOpen={cartOpen} />
+            <Header/>
             <AnimatePresence exitBeforeEnter>
                 <Routes location={location} key={location.pathname}>
                     <Route path={`${BASE_URL}/home`} element={<Home/>}/>
+                    <Route path={`${BASE_URL}/login`} element={<Login/>}/>
                     <Route path={`${BASE_URL}/contacts`} element={<Contacts/>}/>
                     <Route path={`${BASE_URL}/game/:gameId`} element={<GameDetails/>}/>
                     <Route path={BASE_URL} element={<Navigate to={`${BASE_URL}/home`} />}/>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,44 @@
 import {Link} from "react-router-dom";
-import {FormEvent, useContext, useState} from "react";
+import React, {FormEvent, useContext, useState} from "react";
 import {SearchContext, SearchContextType} from "./SearchContext";
 import {RiSearchLine} from "react-icons/ri";
 import {BASE_URL} from "../index";
+import {Cart} from "./Cart";
+import {useClickOutside} from "./useClickOutside";
 
-type Props = {
-    handleCartClick: () => void
-}
-
-function Header({handleCartClick}: Props) {
+function Header() {
     const {updateSearchText} = useContext(SearchContext) as SearchContextType;
+    const [cartOpen, setCartOpen] = useState(false)
     const [text, setText] = useState('')
+    const ref = React.createRef<HTMLDivElement>()
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
         updateSearchText(text);
     }
 
+    useClickOutside(ref, () => setCartOpen(false))
+
     return (
-        <header>
-            <div>
-                <Link to={`${BASE_URL}`}>Gaming store</Link>
-                <Link to={`${BASE_URL}/contacts`}>Contact</Link>
-            </div>
-            <form className='search' onSubmit={handleSubmit}>
-                <input type="text" onChange={e => setText(e.target.value)}/>
-                <button type='submit'>
-                    <RiSearchLine />
-                </button>
-            </form>
-            <span onClick={e => {
-                e.stopPropagation();
-                handleCartClick()
-            }} className='hover-lighter'>cart icon</span>
-        </header>
+        <>
+            <Cart ref={ref} cartOpen={cartOpen}/>
+            <header>
+                <div>
+                    <Link to={`${BASE_URL}`}>Gaming store</Link>
+                    <Link to={`${BASE_URL}/contacts`}>Contact</Link>
+                </div>
+                <form className='search' onSubmit={handleSubmit}>
+                    <input type="text" onChange={e => setText(e.target.value)}/>
+                    <button type='submit'>
+                        <RiSearchLine/>
+                    </button>
+                </form>
+                <span onClick={e => {
+                    e.stopPropagation();
+                    setCartOpen(true)
+                }} className='hover-lighter'>cart icon</span>
+            </header>
+        </>
+
     )
 }
 
